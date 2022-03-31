4552ee98668c5bb79a27213a0587383655228fbb
Josh <jdserpas@gmail.com>
2022-03-31 13:46:12 -0500

    :wrench: updated eslint tab/space rules

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hello, World!</h1>
             </div>
-        )
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import App from "./App.js";
 
-const containter = document.getElementById('root');
+const containter = document.getElementById("root");
 const root = ReactDOMClient.createRoot(containter);
 
 root.render(<App/>);
