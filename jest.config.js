133e4ed1347db59f18009b7b03d5076d48d36c71
Josh <jdserpas@gmail.com>
2022-03-31 16:50:46 -0500

    :wrench: configured mocking for css imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
 
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/setUpTests.js"]
+  setupFilesAfterEnv: ["<rootDir>/setUpTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
