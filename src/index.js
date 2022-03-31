3c32b23129291b85b43edfb2b0ec199864cfcd1c
Josh <jdserpas@gmail.com>
2022-03-31 13:32:11 -0500

    :tada: Created dir struct set up initial React env

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import App from "./App.js";
+
+const containter = document.getElementById('root');
+const root = ReactDOMClient.createRoot(containter);
+
+root.render(<App/>);
