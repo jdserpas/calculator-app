108ee8d9b697381cef9a943c4b3b4a12cbd01b60
Josh <jdserpas@gmail.com>
2022-04-03 15:29:05 -0500

    :recycle: renamed test imports after last change

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from "../App";
+import App from "../Components/App";
 
 describe("App Component", ()=> {
   
