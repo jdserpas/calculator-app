2ab3b20b22dc1b02965580b95f5a73baa005e18c
Josh <jdserpas@gmail.com>
2022-04-03 18:21:50 -0500

    :construction: implemented subtract option

--- src/Components/Selector.js
+++ src/Components/Selector.js
@@ -1,13 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Selector = () => {
+function Selector({options}) {
+  const ops = options.map((op) => 
+    <option key={op.key} value={op.value}>
+      {op.value}
+    </option>
+  );
+  
   return(
     <>
       <select data-testid="drop-down">
-        <option value="+">+</option>
+        {ops}
       </select>
     </>
   );
+}
+
+Selector.propTypes = {
+  options: PropTypes.array
+};
+
+Selector.defaultProps = {
+  options: [
+    {value: "plus",
+      key: 1},
+    {value: "minus",
+      key: 2}
+  ]
 };
 
 export default Selector;
