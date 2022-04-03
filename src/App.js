36734b1e257eddef4e5769b770a42a5aaf3a2c71
Josh <jdserpas@gmail.com>
2022-04-03 11:19:37 -0500

    :construction: added state to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component} from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputOne: 0,
+      inputTwo: 0,
+    };
+  }
+
   render() {
     return(
       <div className="App">
         <h1>Hello, World!</h1>
-        <input type="number" value="0" data-testid="input-one" />
-        <input type="number" value="0" data-testid="input-two" />
+        <input type="number" 
+          value={this.state.inputOne} 
+          data-testid="input-one" />
+        <input type="number" 
+          value={this.state.inputTwo} 
+          data-testid="input-two" />
       </div>
     );
   }
