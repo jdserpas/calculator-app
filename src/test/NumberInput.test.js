ab0f6d4e2675de857a949fb9edc1166b7618c3a3
Josh <jdserpas@gmail.com>
2022-04-03 16:22:31 -0500

    :white_check_mark: checking if value/state changes

--- src/test/NumberInput.test.js
+++ src/test/NumberInput.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import NumberInput from "../Components/NumberInput";
 
@@ -11,5 +11,17 @@ describe("NumberInput Component", () => {
   it("has an initial value of 0", ()=> {
     const {getByTestId} = render(<NumberInput />);
     expect(getByTestId("input-field")).toHaveValue(0);
+  });
+  it("changes value after onChange event fires", ()=> {
+    const {getByTestId} = render(<NumberInput />);
+    const component = getByTestId("input-field");
+
+    fireEvent.change(component, {
+      target: {
+        value: 3
+      }
+    });
+
+    expect(component).toHaveValue(3);
   });
 });
