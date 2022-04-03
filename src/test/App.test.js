e7957b9e55d7d8bcbeb3e8246682eba8fd5a21c0
Josh <jdserpas@gmail.com>
2022-04-03 02:49:11 -0500

    :white_check_mark: added test for checkboxes

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 
 describe("App Component", ()=> {
   it("renders a Hello World header", ()=> {
     const {getByText} = render(<App />);
     expect(getByText("Hello, World!")).toBeInTheDocument();
+  });
+
+  it("renders 2 textboxes", ()=> {
+      const {getByTestId} = render(<App />);
+      //const boxOne = getByTestId("input-one");
+      //const boxTwo = getByTestId("input-two");
+
+      expect(getByTestId("input-one")).toBeInTheDocument();
+      expect(getByTestId("input-two")).toBeInTheDocument();
   });
 });
