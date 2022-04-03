e3595457f1d5c5781a685630efd75fdc5269cbc8
Josh <jdserpas@gmail.com>
2022-04-03 15:53:32 -0500

    :white_check_mark: test only checks for child

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -12,30 +12,10 @@ describe("App Component", ()=> {
 
   /**********Text boxes**************/
   it("renders 2 textboxes", ()=> {
-    const {getByTestId} = render(<App />);
+    const {getAllByTestId, getByTestId} = render(<App />);
+    const Container = getByTestId("app-containter");
+    const Child = getAllByTestId("input-field");
     
-    expect(getByTestId("input-one")).toBeInTheDocument();
-    expect(getByTestId("input-two")).toBeInTheDocument();
-  });
-
-  test("two text boxes in component default to 0", ()=> {
-    const {getByTestId} = render(<App />);
-
-    expect(getByTestId("input-one")).toHaveValue(0);
-    expect(getByTestId("input-two")).toHaveValue(0);
-  });
-
-  test("text boxes values change", ()=> {
-    const {getByTestId} = render(<App />);
-    const component = getByTestId("input-one");
-    const newVal = 3;
-
-    fireEvent.change(component, {
-      target: {
-        value: newVal
-      }
-    });
-
-    expect(component).toHaveValue(newVal);
+    expect(Container).toContainElement(Child[0]);
   });
 });
