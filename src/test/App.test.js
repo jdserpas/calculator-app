62b95c42ec3f4e6dce12cd81d29359fe868f3db0
Josh <jdserpas@gmail.com>
2022-04-03 11:10:31 -0500

    :white_check_mark: made test for text box 1 change

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 
 describe("App Component", ()=> {
+  
   it("renders a Hello World header", ()=> {
     const {getByText} = render(<App />);
     expect(getByText("Hello, World!")).toBeInTheDocument();
   });
 
+  /**********Text boxes**************/
   it("renders 2 textboxes", ()=> {
     const {getByTestId} = render(<App />);
-    //const boxOne = getByTestId("input-one");
-    //const boxTwo = getByTestId("input-two");
-
+    
     expect(getByTestId("input-one")).toBeInTheDocument();
     expect(getByTestId("input-two")).toBeInTheDocument();
   });
@@ -23,5 +23,19 @@ describe("App Component", ()=> {
 
     expect(getByTestId("input-one")).toHaveValue(0);
     expect(getByTestId("input-two")).toHaveValue(0);
+  });
+
+  test("text boxes values change", ()=> {
+    const {getByTestId} = render(<App />);
+    const component = getByTestId("input-one");
+    const newVal = "3"
+
+    fireEvent.change(component, {
+      target: {
+        value: newVal
+      }
+    });
+
+    expect(component.textContent).toBe(newVal);
   });
 });
