469675092ea1f8cc62022c93325a6bce0db06e95
Josh <jdserpas@gmail.com>
2022-04-03 17:27:35 -0500

    :white_check_mark: Created test for selector

---
+++ src/test/Selector.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Selector from "../Components/Selector";
+
+describe("Selector compnent", ()=> {
+  beforeEach(()=> {
+    render(<Selector />);
+  });
+
+  it("renders a drop down", ()=> {
+    const component = screen.getByTestId("drop-down");
+    expect(component).toBeInTheDocument();
+  });
+});
